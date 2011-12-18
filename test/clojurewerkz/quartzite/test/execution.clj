4193d4a331c4d0f0d6a6c42999d94082186e58f0
Michael S. Klishin <michael@novemberain.com>
2011-12-18 23:28:08 +0400

    Compile

--- test/clojurewerkz/quartzite/test/execution.clj
+++ test/clojurewerkz/quartzite/test/execution.clj
@@ -5,8 +5,5 @@
 
 
 (deftest test-basic-periodic-execution
-  (let [latch   (java.util.concurrent.CountDownLatch. 10)
-        job     (qj/build (qj/with-identity    "basic.job1")
-                          (qj/with-group       "basic.group1")
-                          (qj/with-description "A description"))]
-    (is false)))
+  (let [latch   (java.util.concurrent.CountDownLatch. 10)]
+    (is false "We haven't gotten to these tests yet")))
