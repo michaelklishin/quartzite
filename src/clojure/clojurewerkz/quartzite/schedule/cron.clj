49669932452aa8d9df08858f2761fa01a99d39f1
Michael S. Klishin <michael@novemberain.com>
2011-12-29 01:08:47 +0400

    Support key cron schedule DSL verbs

--- src/clojure/clojurewerkz/quartzite/schedule/cron.clj
+++ src/clojure/clojurewerkz/quartzite/schedule/cron.clj
@@ -11,6 +11,14 @@
   [^long hour ^long minute]
   (CronScheduleBuilder/dailyAtHourAndMinute hour minute))
 
+(defn weekly-on-day-and-hour-and-minute
+  [^long day-of-week ^long hour ^long minute]
+  (CronScheduleBuilder/weeklyOnDayAndHourAndMinute day-of-week hour minute))
+
+(defn monthly-on-day-and-hour-and-minute
+  [^long day-of-month ^long hour ^long minute]
+  (CronScheduleBuilder/monthlyOnDayAndHourAndMinute day-of-month hour minute))
+
 
 (defn finalize
   [^CronScheduleBuilder ssb]
--- test/clojurewerkz/quartzite/test/schedule/cron.clj
+++ test/clojurewerkz/quartzite/test/schedule/cron.clj
@@ -3,7 +3,7 @@
   (:use [clojure.test]
         [clojurewerkz.quartzite.schedule cron]
         [clj-time.core :only [date-time]])
-  (:import [org.quartz CronScheduleBuilder]
+  (:import [org.quartz CronScheduleBuilder DateBuilder]
            [org.quartz.impl.triggers CronTriggerImpl]
            [org.joda.time DateTime]))
 
@@ -29,3 +29,27 @@
     (is (= "0 0 15 ? * *" (.getCronExpression sched)))
     (is (.willFireOn sched (.toCalendar d1 nil) true))
     (is (.willFireOn sched (.toCalendar d2 nil) true))))
+
+
+(deftest test-cron-schedule-dsl-example3
+  (let [^DateTime d1     (date-time 2012 1  5  15)
+        ^DateTime d2     (date-time 2012 1  6  15)
+        ^DateTime d3     (date-time 2012 1  12  15)
+        ^CronTriggerImpl sched (schedule
+                                 (weekly-on-day-and-hour-and-minute DateBuilder/THURSDAY 15 0)
+                                 (finalize))]
+    (is (= "0 0 15 ? * 5" (.getCronExpression sched)))
+    (is (.willFireOn sched (.toCalendar d1 nil) true))
+    (is (not (.willFireOn sched (.toCalendar d2 nil) true)))
+    (is (.willFireOn sched (.toCalendar d3 nil) true))))
+
+
+(deftest test-cron-schedule-dsl-example4
+  (let [^DateTime d1     (date-time 2012 1  7  15)
+        ^DateTime d2     (date-time 2012 1  3  15)
+        ^CronTriggerImpl sched (schedule
+                                 (monthly-on-day-and-hour-and-minute 7 15 0)
+                                 (finalize))]
+    (is (= "0 0 15 7 * ?" (.getCronExpression sched)))
+    (is (.willFireOn sched (.toCalendar d1 nil) true))
+    (is (not (.willFireOn sched (.toCalendar d2 nil) true)))))
