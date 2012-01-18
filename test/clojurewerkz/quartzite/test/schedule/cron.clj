b3573b6b1431cf410655e90c58f0b18eda736de7
Michael S. Klishin <michael@novemberain.com>
2012-01-18 19:05:34 +0400

    More examples for "on the last day of the month" and "on the next to last day of the month" schedules

--- test/clojurewerkz/quartzite/test/schedule/cron.clj
+++ test/clojurewerkz/quartzite/test/schedule/cron.clj
@@ -68,8 +68,29 @@
         ^DateTime d4     (date-time 2015 3  31  15)
         ^DateTime d5     (date-time 2015 4  30  15)
         ^DateTime d6     (date-time 2015 4  28  15)
+        ^DateTime d7     (date-time 2016 2  29  15)
+        ^DateTime d8     (date-time 2017 2  28  15)
         ^CronTriggerImpl sched (schedule
                                 (cron-schedule "0 0 15 L * ?")
+                                 (finalize))]
+    (is (not (.willFireOn sched (.toCalendar d1 nil) true)))
+    (is (.willFireOn sched (.toCalendar d2 nil) true))
+    (is (.willFireOn sched (.toCalendar d3 nil) true))
+    (is (.willFireOn sched (.toCalendar d4 nil) true))
+    (is (.willFireOn sched (.toCalendar d5 nil) true))
+    (is (not (.willFireOn sched (.toCalendar d6 nil) true)))
+    (is (.willFireOn sched (.toCalendar d7 nil) true))
+    (is (.willFireOn sched (.toCalendar d8 nil) true))))
+
+(deftest test-cron-schedule-next-to-last-day-of-the-month
+  (let [^DateTime d1     (date-time 2015 1  7  15)
+        ^DateTime d2     (date-time 2015 1  30  15)
+        ^DateTime d3     (date-time 2015 2  27  15)
+        ^DateTime d4     (date-time 2015 3  30  15)
+        ^DateTime d5     (date-time 2015 4  29  15)
+        ^DateTime d6     (date-time 2015 5  31  15)
+        ^CronTriggerImpl sched (schedule
+                                (cron-schedule "0 0 15 L-1 * ?")
                                  (finalize))]
     (is (not (.willFireOn sched (.toCalendar d1 nil) true)))
     (is (.willFireOn sched (.toCalendar d2 nil) true))
