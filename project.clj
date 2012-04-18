2b5ff47ff323ded96e760a83dd8d35bad0ef943e
Michael S. Klishin <michael@novemberain.com>
2012-04-18 15:50:06 +0400

    1.0.0-beta4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/quartzite "1.0.0-SNAPSHOT"
+(defproject clojurewerkz/quartzite "1.0.0-beta4"
   :description "Quarzite is a thin Clojure layer on top the Quartz Scheduler"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
@@ -7,18 +7,14 @@
                  [clj-time "0.3.7"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]},
-             :dev {:resource-paths ["test/resources"],
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :dev {:resource-paths ["test/resources"]
                    :dependencies [[org.clojure/tools.logging "0.2.3" :exclusions [org.clojure/clojure]]
-                                  [org.slf4j/slf4j-simple "1.6.2"]
-                                  [org.slf4j/slf4j-api "1.6.2"]
-                                  [log4j "1.2.16" :exclusions [javax.mail/mail
-                                                               javax.jms/jms
-                                                               com.sun.jdmk/jmxtools
-                                                               com.sun.jmx/jmxri]]]}}
-  :aliases { "all" ["with-profile" "dev:dev,1.4"] }
-  :repositories {"clojure-releases" "http://build.clojure.org/releases",
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases",
-                             :snapshots false,
-                             :releases {:checksum :fail, :update :always}}}
+                                  [org.slf4j/slf4j-simple    "1.6.2"]
+                                  [org.slf4j/slf4j-api       "1.6.2"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.4"]}
+  :repositories {"clojure-releases" "http://build.clojure.org/releases"
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}}
   :warn-on-reflection true)
