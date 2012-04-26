b360c459be6ade18c7e843f5f79291b6787cab3d
Michael S. Klishin <michael@novemberain.com>
2012-04-26 20:45:17 +0400

    Enable local 1.5-SNAPSHOT testing

--- project.clj
+++ project.clj
@@ -11,13 +11,17 @@
                    :focus   :focus
                    :default (constantly true)}
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :dependencies [[org.clojure/tools.logging "0.2.3" :exclusions [org.clojure/clojure]]
                                   [org.slf4j/slf4j-simple    "1.6.2"]
                                   [org.slf4j/slf4j-api       "1.6.2"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4"]}
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]
+            "ci"  ["with-profile" "dev:dev,1.4"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
-                             :releases {:checksum :fail :update :always}}}
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                               :snapshots true
+                               :releases {:checksum :fail :update :always}}}
   :warn-on-reflection true)
