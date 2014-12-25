d55fee31146dc4aded50a0687de40ceee6418f74
Michael Klishin <michael@clojurewerkz.org>
2014-12-25 15:11:42 +0300

    Switch clojurewerkz.quartzite.scheduler fns to accept an explicit scheduler argument

--- test/clojurewerkz/quartzite/test/execution_test.clj
+++ test/clojurewerkz/quartzite/test/execution_test.clj
@@ -7,14 +7,9 @@
             [clojurewerkz.quartzite.schedule.calendar-interval :as calin]
             [clojure.test :refer :all]
             [clojurewerkz.quartzite.conversion :refer :all]
-            [clojurewerkz.quartzite.test.helper :refer :all]
             [clj-time.core :refer [now seconds from-now]])
   (:import java.util.concurrent.CountDownLatch
            org.quartz.impl.matchers.GroupMatcher))
-
-(println (str "Using Clojure version " *clojure-version*))
-
-(wrap-fixtures)
 
 ;;
 ;; Case 1
@@ -27,9 +22,9 @@
   (execute [this ctx]
     (.countDown ^CountDownLatch latch1)))
 
-(deftest test-basic-periodic-execution-with-a-job-defined-using-defrecord
-  (is (sched/started?))
-  (let [job     (j/build
+(deftest ^:focus test-basic-periodic-execution-with-a-job-defined-using-defrecord
+  (let [s       (-> (sched/initialize) sched/start)
+        job     (j/build
                  (j/of-type JobA)
                  (j/with-identity "clojurewerkz.quartzite.test.execution-test.job1" "tests"))
         trigger  (t/build
@@ -37,14 +32,18 @@
                   (t/with-schedule (s/schedule
                                     (s/with-repeat-count 10)
                                     (s/with-interval-in-milliseconds 200))))]
-    (sched/schedule job trigger)
-    (let [j (sched/get-job (j/key "clojurewerkz.quartzite.test.execution-test.job1" "tests"))
+    (is (sched/started? s))
+    (sched/schedule s job trigger)
+    (let [j (sched/get-job s (j/key "clojurewerkz.quartzite.test.execution-test.job1" "tests"))
           m (from-job-detail j)]
       (is j)
       (is (:key m))
       (is (nil? (:description m)))
       (is (:job-data m)))
-    (.await ^CountDownLatch latch1)))
+    (try
+      (.await ^CountDownLatch latch1)
+      (finally
+        (sched/shutdown s)))))
 
 
 
@@ -59,8 +58,8 @@
   (swap! counter2 inc))
 
 (deftest test-unscheduling-of-a-job-defined-using-defjob
-  (is (sched/started?))
-  (let [jk      (j/key "clojurewerkz.quartzite.test.execution-test.job2"     "tests")
+  (let [s       (-> (sched/initialize) sched/start)
+        jk      (j/key "clojurewerkz.quartzite.test.execution-test.job2"     "tests")
         tk      (t/key "clojurewerkz.quartzite.test.execution-test.trigger2" "tests")
         job     (j/build
                  (j/of-type JobB)
@@ -72,29 +71,30 @@
                   (t/with-schedule (s/schedule
                                     (s/with-repeat-count 10)
                                     (s/with-interval-in-milliseconds 400))))]
-    (sched/schedule job trigger)
-    (is (sched/all-scheduled? jk tk))
-    (is (not (empty? (sched/get-triggers [tk]))))
-    (is (not (empty? (sched/get-jobs [jk]))))
-    (let [t (sched/get-trigger tk)
+    (sched/schedule s job trigger)
+    (is (sched/all-scheduled? s jk tk))
+    (is (not (empty? (sched/get-triggers s [tk]))))
+    (is (not (empty? (sched/get-jobs s [jk]))))
+    (let [t (sched/get-trigger s tk)
           m (from-trigger t)]
       (is t)
       (is (:key m))
       (is (:description m))
       (is (:start-time m))
       (is (:next-fire-time m)))
-    (is (sched/get-job jk))
-    (is (nil? (sched/get-job (j/key "ab88fsyd7f" "k28s8d77s"))))
-    (is (nil? (sched/get-trigger (t/key "ab88fsyd7f"))))
-    (is (not (empty? (sched/get-trigger-keys (m/group-equals "tests")))))
-    (is (not (empty? (sched/get-matching-triggers (m/group-equals "tests")))))
-    (is (not (empty? (sched/get-job-keys (m/group-equals "tests")))))
-    (is (not (empty? (sched/get-matching-jobs (m/group-equals "tests")))))
+    (is (sched/get-job s jk))
+    (is (nil? (sched/get-job s (j/key "ab88fsyd7f" "k28s8d77s"))))
+    (is (nil? (sched/get-trigger s (t/key "ab88fsyd7f"))))
+    (is (not (empty? (sched/get-trigger-keys s (m/group-equals "tests")))))
+    (is (not (empty? (sched/get-matching-triggers s (m/group-equals "tests")))))
+    (is (not (empty? (sched/get-job-keys s (m/group-equals "tests")))))
+    (is (not (empty? (sched/get-matching-jobs s (m/group-equals "tests")))))
     (Thread/sleep 2000)
-    (sched/delete-trigger tk)
-    (is (not (sched/all-scheduled? tk jk)))
+    (sched/delete-trigger s tk)
+    (is (not (sched/all-scheduled? s tk jk)))
     (Thread/sleep 2000)
-    (is (< @counter2 7))))
+    (is (< @counter2 7))
+    (sched/shutdown s)))
 
 
 
@@ -109,8 +109,8 @@
   (swap! counter3 inc))
 
 (deftest test-manual-triggering-of-a-job-defined-using-defjob
-  (is (sched/started?))
-  (let [jk      (j/key "clojurewerkz.quartzite.test.execution-test.job3" "tests")
+  (let [s       (-> (sched/initialize) sched/start)
+        jk      (j/key "clojurewerkz.quartzite.test.execution-test.job3" "tests")
         tk      (t/key "clojurewerkz.quartzite.test.execution-test.trigger3" "tests")
         job     (j/build
                  (j/of-type JobC)
@@ -121,10 +121,11 @@
                   (t/with-schedule (s/schedule
                                     (s/with-repeat-count 10)
                                     (s/with-interval-in-seconds 2))))]
-    (sched/schedule job trigger)
-    (sched/trigger jk)
+    (sched/schedule s job trigger)
+    (sched/trigger s jk)
     (Thread/sleep 500)
-    (is (= 2 @counter3))))
+    (is (= 2 @counter3))
+    (sched/shutdown s)))
 
 
 ;;
@@ -139,8 +140,8 @@
                   (from-job-data (.getMergedJobDataMap ctx)))))
 
 (deftest test-job-data-access
-  (is (sched/started?))
-  (let [jk      (j/key "clojurewerkz.quartzite.test.execution-test.job4" "tests")
+  (let [s       (-> (sched/initialize) sched/start)
+        jk      (j/key "clojurewerkz.quartzite.test.execution-test.job4" "tests")
         tk      (t/key "clojurewerkz.quartzite.test.execution-test.trigger4" "tests")
         job     (j/build
                  (j/of-type JobD)
@@ -152,10 +153,11 @@
                   (t/with-schedule (s/schedule
                                     (s/with-repeat-count 10)
                                     (s/with-interval-in-seconds 2))))]
-    (sched/schedule job trigger)
-    (sched/trigger jk)
+    (sched/schedule s job trigger)
+    (sched/trigger s jk)
     (Thread/sleep 1000)
-    (is (= "job-value" (get @value4 "job-key")))))
+    (is (= "job-value" (get @value4 "job-key")))
+    (sched/shutdown s)))
 
 
 ;;
@@ -170,8 +172,8 @@
     (swap! counter5 + i)))
 
 (deftest test-job-pausing-resuming-and-unscheduling
-  (is (sched/started?))
-  (let [jk      (j/key "clojurewerkz.quartzite.test.execution-test.job5" "tests.jobs.unscheduling")
+  (let [s       (-> (sched/initialize) sched/start)
+        jk      (j/key "clojurewerkz.quartzite.test.execution-test.job5" "tests.jobs.unscheduling")
         tk      (t/key "clojurewerkz.quartzite.test.execution-test.trigger5" "tests.jobs.unscheduling")
         job     (j/build
                  (j/of-type JobE)
@@ -183,27 +185,28 @@
                   (t/with-schedule (s/schedule
                                     (s/with-repeat-count 10)
                                     (s/with-interval-in-seconds 1))))]
-    (sched/schedule job trigger)
-    (sched/pause-job jk)
-    (sched/resume-job jk)
-    (sched/pause-jobs (GroupMatcher/groupEquals "tests.jobs.unscheduling"))
-    (sched/resume-jobs (GroupMatcher/groupEquals "tests.jobs.unscheduling"))
-    (sched/pause-trigger tk)
-    (sched/resume-trigger tk)
-    (sched/pause-triggers (GroupMatcher/groupEquals "tests.triggers.unscheduling"))
-    (sched/resume-triggers (GroupMatcher/groupEquals "tests.triggers.unscheduling"))
-    (sched/pause-all!)
-    (sched/resume-all!)
-    (sched/delete-trigger tk)
+    (sched/schedule s job trigger)
+    (sched/pause-job s jk)
+    (sched/resume-job s jk)
+    (sched/pause-jobs s (GroupMatcher/groupEquals "tests.jobs.unscheduling"))
+    (sched/resume-jobs s (GroupMatcher/groupEquals "tests.jobs.unscheduling"))
+    (sched/pause-trigger s tk)
+    (sched/resume-trigger s tk)
+    (sched/pause-triggers s (GroupMatcher/groupEquals "tests.triggers.unscheduling"))
+    (sched/resume-triggers s (GroupMatcher/groupEquals "tests.triggers.unscheduling"))
+    (sched/pause-all! s)
+    (sched/resume-all! s)
+    (sched/delete-trigger s tk)
     (Thread/sleep 300)
-    (sched/delete-triggers [tk])
-    (sched/delete-job jk)
-    (sched/delete-jobs [jk])
+    (sched/delete-triggers s [tk])
+    (sched/delete-job s jk)
+    (sched/delete-jobs s [jk])
     (Thread/sleep 3000)
     ;; with start-now policty some executions
     ;; manages to get through. In part this test is supposed
     ;; to demonstrate it as much as test unscheduling/pausing functions. MK.
-    (is (< @counter5 15))))
+    (is (< @counter5 15))
+    (sched/shutdown s)))
 
 
 ;;
@@ -217,16 +220,19 @@
   (.countDown ^CountDownLatch latch6))
 
 (deftest test-basic-periodic-execution-with-calendar-interval-schedule
-  (is (sched/started?))
-  (let [job     (j/build
+  (let [s       (-> (sched/initialize) sched/start)
+        job     (j/build
                  (j/of-type JobF)
                  (j/with-identity "clojurewerkz.quartzite.test.execution-test.job6" "tests"))
         trigger  (t/build
                   (t/start-now)
                   (t/with-schedule (calin/schedule
                                     (calin/with-interval-in-seconds 2))))]
-    (sched/schedule job trigger)
-    (.await ^CountDownLatch latch6)))
+    (sched/schedule s job trigger)
+    (try
+      (.await ^CountDownLatch latch6)
+      (finally
+        (sched/shutdown s)))))
 
 
 
@@ -241,20 +247,21 @@
   (swap! counter7 inc))
 
 (deftest test-double-scheduling
-  (is (sched/started?))
-  (let [job     (j/build
+  (let [s       (-> (sched/initialize) sched/start)
+        job     (j/build
                  (j/of-type JobG)
                  (j/with-identity "clojurewerkz.quartzite.test.execution-test.job7" "tests"))
         trigger  (t/build
                   (t/start-at (-> 2 seconds from-now))
                   (t/with-schedule (calin/schedule
                                     (calin/with-interval-in-seconds 2))))]
-    (is (sched/schedule job trigger))
+    (is (sched/schedule s job trigger))
     ;; schedule will raise an exception
     (is (thrown?
          org.quartz.ObjectAlreadyExistsException
-         (sched/schedule job trigger)))
+         (sched/schedule s job trigger)))
     ;; but maybe-schedule will not
-    (is (not (sched/maybe-schedule job trigger)))
+    (is (not (sched/maybe-schedule s job trigger)))
     (Thread/sleep 7000)
-    (is (= 3 @counter7))))
+    (is (= 3 @counter7))
+    (sched/shutdown s)))
