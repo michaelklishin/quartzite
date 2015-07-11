e152a5bea21f4e315278d700f7f79af8092c827e
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:16:59 +0300

    Clojure master is now 1.8.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                    :default (constantly true)}
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :dependencies [[org.clojure/tools.logging "0.2.3" :exclusions [org.clojure/clojure]]
                                   [org.slf4j/slf4j-log4j12   "1.6.4"]]}}
